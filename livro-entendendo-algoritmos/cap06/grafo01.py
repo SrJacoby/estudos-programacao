d76b059635b13a4e4ef3d15c5aab1344b83097b6
SrJacoby <luizjacoby294@gmail.com>
2025-09-02 17:43:30 -0300

    adicionando cap. 06

---
+++ livro-entendendo-algoritmos/cap06/grafo01.py
@@ -0,0 +1,12 @@
+#exemplo de grafo em Python utilizando uma tabela hash
+
+grafo = {}
+grafo["voce"] = ["alice", "bob", "claire"]
+grafo["bob"] = ["anuj", "peggy"]
+grafo["alice"] = ["peggy"]
+grafo["claire"] = ["thom", "jonny"]
+grafo["anuj"] = []
+grafo["peggy"] = []
+grafo["thom"] = []
+grafo["jonny"] = []
+
