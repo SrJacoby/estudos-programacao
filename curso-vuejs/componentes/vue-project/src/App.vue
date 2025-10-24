d95d750d7e68935dd3ffc77f4f4513be5966bfd4
SrJacoby <luizjacoby294@gmail.com>
2025-10-24 17:19:10 -0300

    desafio prático

---
+++ curso-vuejs/componentes/vue-project/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Meu App Vue</h1>
+        <Contador />
+        <Mensagem />
+    </div>
+</template>
+
+<script>
+    import Contador from './Contador.vue'
+    import Mensagem from './Mensagem.vue'
+
+    export default{
+        components: {
+            Contador,
+            Mensagem
+        }
+    }
+</script>
