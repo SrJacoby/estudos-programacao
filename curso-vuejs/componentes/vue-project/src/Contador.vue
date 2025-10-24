b6bbc4d3c2caddb50641151cab28201d5221efc8
SrJacoby <luizjacoby294@gmail.com>
2025-10-24 17:23:44 -0300

    atualizando arquivos

---
+++ curso-vuejs/componentes/vue-project/src/Contador.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>Valor atual: {{ count }}</p>
+        <button @click="count++">Somar</button>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                count: 0
+            }
+        }
+    }
+</script>
---
+++ curso-vuejs/componentes/vue-project/src/Mensagem.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <button @click="mostrar = !mostrar">
+            {{mostrar ? "Olá vue!" : "Bem-vindo ao curso!"}}
+        </button>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return {
+                mostrar: true
+            }
+        }
+    }
+</script>
